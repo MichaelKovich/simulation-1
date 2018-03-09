f9f3b955c172c5d9615d6fcb3791406080dcc94f
Michael Kovich <kovich@michaelkovich.com>
2018-03-09 10:09:40 -0600

    Initial Commit.

---
+++ server/index.js
@@ -0,0 +1,35 @@
+// Import Absolute Dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const massive = require('massive');
+const cors = require('cors');
+require('dotenv').config();
+
+// Import Controller
+// REQUIRE CONTROLLER HERE
+
+// Instantiate Express
+const app = express();
+// Used to parse information
+app.use(bodyParser.json());
+// Used to apply some level of security to the application
+app.use(cors());
+
+// Identify static files to be served by Express:
+app.use(express.static('../public/build'));
+
+// Invoking massive with the connection_string in the .env file
+massive(process.env.CONNECTION_STRING)
+  .then((dbInstance) => {
+    app.set('db', dbInstance);
+  })
+  .catch(err => console.log(err));
+
+// ROUTING HERE
+// app.get
+// app.post
+// app.put
+// app.delete
+
+// LISTENING HERE
+app.listen(process.env.PORT || 3001, () => console.log('Listening!'));
